1d0c43f6536503486d51b64e931dc38f56426cd3
Vivaan Saroj <149756428+immortalvivaan461@users.noreply.github.com>
2025-09-12 23:48:20 +0530

    Update Navbar.jsx

--- frontend/src/components/UI/Navbar.jsx
+++ frontend/src/components/UI/Navbar.jsx
@@ -1,7 +1,7 @@
 import './Navbar.css';
 
 import TextType from './TextType';
-import logo from "../../assets/logo.png";
+import logo from "/src/assets/logo.png";
 
 import React, { useState, useEffect } from "react";
 import { NavLink } from 'react-router-dom';
