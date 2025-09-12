c4a6765a9e6bfed248e9586559bb95c31bdb527d
Vivaan Saroj <149756428+immortalvivaan461@users.noreply.github.com>
2025-09-12 23:55:32 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,10 @@
 import './App.css'
-// import Navbar from './components/UI/Navbar'
-// import Home from './components/Home'
-// import Footer from './components/UI/Footer'
-
-
 
 function App() {
 
 
   return (
     <>
-      {/* <Navbar />
-      <Home />
-      <hr />
-      <Footer /> */}
     </>
   )
 }
