95759e0461ca43d2b5188da4fa06cb2e15db18f2
Vivaan Saroj <149756428+immortalvivaan461@users.noreply.github.com>
2025-09-13 00:05:22 +0530

    Update Footer.jsx

--- frontend/src/components/UI/Footer.jsx
+++ frontend/src/components/UI/Footer.jsx
@@ -1,5 +1,5 @@
 import './Footer.css'
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faTwitter, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { NavLink } from 'react-router-dom';
